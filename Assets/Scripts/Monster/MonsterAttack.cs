bf2db0a464d3333a2356766a58e06faa5ac3bc3c
rbase0218 <rbase@naver.com>
2025-03-27 23:57:39 +0900

    Fix :  몬스터 공격 스크립트 제거

--- Assets/Scripts/Monster/MonsterAttack.cs
+++ Assets/Scripts/Monster/MonsterAttack.cs
@@ -18,34 +18,16 @@ public class MonsterAttack : MonoBehaviour
     
     private void Update()
     {
-        if (!_canAttack)
-        {
-            _attackCooldown -= Time.deltaTime;
-            if (_attackCooldown <= 0f)
-                _canAttack = true;
-        }
-        
         TryAttack();
     }
     
     private void TryAttack()
     {
-        if (_playerTransform == null || !_canAttack)
-            return;
-        
-        float distanceToPlayer = Vector2.Distance(transform.position, _playerTransform.position);
-        
-        if (distanceToPlayer <= _status.attackRange)
-            Attack();
+
     }
     
     private void Attack()
     {
-        _canAttack = false;
-        _attackCooldown = 1f / _status.attackSpeed;
-        
-        var pc = _playerTransform.GetComponent<PlayerController>();
-        
-        pc.GetStatus().TakeDamage(_status.damage);
+
     }
 }
